e3d92a9ae5d6136376e6224097d99ef1f76eb8b9
Diego Soria <diegosoriaarg@gmail.com>
2021-10-11 13:41:16 -0300

    Agregando el proyecto

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Nuevo visitante??</div>
+
+                    <div class="card-body">
+                        Para entrar en el sitio, debes <a href="/login">logearte</a><br>
+                        Ó <a href="/register">registrate</a>, y se parte de esta comunidad...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
